84e79170c686417cfa14b9042ab5226d3d7c2e86
cse18107 <cse18107@cemk.ac.in>
2022-06-13 17:14:19 +0530

    complete project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+import { ethers } from 'ethers';
 
 function App() {
+
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [defaultAccount, setDefaultAccount] = useState(null);
+  const [userBalance, setUserBalance] = useState(null);
+  const [connButtonText, setConnButtonText] = useState('Connect Wallet');
+
+  const connectWalletHandler = () => {
+    if(window.ethereum){
+      console.log(window.ethereum);
+      window.ethereum.request({method:'eth_requestAccounts'})
+      .then(result=>{
+        accountChangeHandler(result[0]);
+      })
+    }else{
+      setErrorMessage('Install MetaMask')
+    }
+  }
+
+  const accountChangeHandler = (newAccount) => {
+    setDefaultAccount(newAccount);
+    getUserBalance(newAccount.toString());
+  }
+
+  const getUserBalance = (address) => {
+    window.ethereum.request({method:'eth_getBalance',params:[address,'latest']}).then(balance=>{
+      setUserBalance(ethers.utils.formatEther(balance));
+    })
+  }
+
+  const chainChangedHandler = () => {
+    window.location.reload();
+  }
+
+  window.ethereum.on('accountsChanged', accountChangeHandler);
+
+  window.ethereum.on('chainChanged', chainChangedHandler);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h4>Connection to MetaMask using window.ethereum method</h4>
+     <button onClick={connectWalletHandler}>Connect Wallet</button>
+     <p>Address:{defaultAccount}</p>
+     <p>Balance:{userBalance}</p>
     </div>
   );
 }
